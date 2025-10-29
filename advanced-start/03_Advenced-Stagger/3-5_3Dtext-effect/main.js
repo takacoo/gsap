6c6613fc9ea14bc63f1272b32de87e56ecfd4bf4
ayano takaishi <>
2025-10-29 09:28:16 +0900

    section3完了

--- advanced-start/03_Advenced-Stagger/3-5_3Dtext-effect/main.js
+++ advanced-start/03_Advenced-Stagger/3-5_3Dtext-effect/main.js
@@ -1,4 +1,10 @@
 const duration = 0.5
+const pause = 1
+
+const numberOfTargets = gsap.utils.toArray('.utils > div').length
+const stagger = duration + pause
+
+const delay = stagger * (numberOfTargets - 1) + pause
 
 const tl = gsap.timeline();
 
@@ -10,14 +16,24 @@ tl.from('.utils > div',{
   rotationX:-90,
   // rotationY:-50,
   opacity:0,
-  stagger:duration
+  duration:duration,
+  stagger:{
+    each:stagger,
+    repeat:-1,
+    repeatDelay:delay,
+  }
 })
 
 .to('.utils > div',{
   rotationX:90,
   // rotationY:50,
   opacity:0,
-  stagger:duration,
-},duration)//.5秒後に実行
+  duration:duration,
+  stagger:{
+    each:stagger,
+    repeat:-1,
+    repeatDelay:delay,
+  }
+},stagger)//.5秒後に実行
 
 GSDevTools.create()
--- advanced-start/03_Advenced-Stagger/3-8.practice/main.js
+++ advanced-start/03_Advenced-Stagger/3-8.practice/main.js
@@ -13,18 +13,109 @@ const planet = [
   'pluto'
 ]
 
+const list = planet.map((planetName)=>{
+  return /* html */`
+  <div class="solar_system" data-planet-name="${planetName}">
+    <div class="planet ${planetName}">
+      <div class="overlay"></div>
+      <h2>${planetName}</h2>
+    </div>
+  </div>
+  `
+})
+
+function render(){
+  const space = $('.space');
+
+  list.forEach((planet)=>{
+    space.insertAdjacentHTML('beforeend',planet);
+  })
+}
+
+render();
 
 
+const z = gsap.utils.distribute({
+  base:-18400,
+  amount:18400,
+  from:'end',
+})
+
+gsap.set('.planet',{
+  z:z,
+})
+
+const rightButton = $('.right');
+
+let count = 0;
+let trigger = false;
+
+rightButton.addEventListener('click',()=>{
+
+  if(count > 7) return;
+
+  if(!trigger){
+
+    count++;
+
+    gsap.to('.planet',{
+      z: '+=2300',
+      ease:'power3.inOut',
+      duration:2,
+      onComplete(){
+        trigger = false;
+      }
+    })
+
+    trigger = true;
+  }
+})
+
+const leftButton = $('.left');
+
+leftButton.addEventListener('click',()=>{
+  
+  if(count < 0) return;
+
+  if(!trigger){
+    count--;
+    gsap.to('.planet',{
+    z: '-=2300',
+      ease:'power3.inOut',
+      duration:2,
+      onComplete(){
+        trigger = false;
+      }
+    })
+    trigger = true;
+  }
+})
 
 // map  -> 배열의 능력 (새로운 배열을 반환)
 // forEach -> 배열의 능력 (값을 반환하지 x)
 // insertAdjacentHTML (dom 뿌려주는)
 
+// map - >配列の能力（新しい配列を返す）
+// forEach - >配列の能力（値を返す）
+// insertAdjacentHTML（domに出力する役割）
 
+// const todo = [
+//   '起きる',
+//   '朝食を食べる',
+//   '歯を磨く',
+//   '学校に行く',
+//   '勉強をする',
+//   '宿題をする',
+//   '寝る',
+// ]
 
+// const list = todo.map((item,index)=>{
+//   return `<div data-index="${index}">${item}</div>`
+// })
 
-
-
+// list.forEach((item,index)=>{
+//   console.log(item,index);
+// })
 
 
 
