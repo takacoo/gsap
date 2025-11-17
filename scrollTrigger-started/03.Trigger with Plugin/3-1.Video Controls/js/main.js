bde39f59fc970436c68867efc7b46ab86bfd445a
ayano takaishi <>
2025-11-17 09:38:20 +0900

    3-3-18(有償部分省略)

--- scrollTrigger-started/03.Trigger with Plugin/3-1.Video Controls/js/main.js
+++ scrollTrigger-started/03.Trigger with Plugin/3-1.Video Controls/js/main.js
@@ -1,3 +1,7 @@
-
+const container = document.querySelector('#container');
+const scrollbar = Scrollbar.init(container,{
+  damping: .1,//値が少ないほど滑る
+  alweyShowTracks: true,//スクロールしてなくてもスクロールバー表示
+})
 
 markers()
