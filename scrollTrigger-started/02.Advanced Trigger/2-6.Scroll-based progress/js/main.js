3786572646dd0dabb0767131c1b1b628cca85eaa
ayano takaishi <>
2025-11-20 09:45:55 +0900

    3-3-24まで

--- scrollTrigger-started/02.Advanced Trigger/2-6.Scroll-based progress/js/main.js
+++ scrollTrigger-started/02.Advanced Trigger/2-6.Scroll-based progress/js/main.js
@@ -1,33 +1,17 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ScrollTrigger.create({
+  trigger:'.progressHolder',
+  start:'top ' + (document.querySelector('.section01').offsetHeight - 150),
+  endTrigger:'.section03',
+  end:'bottom bottom',
+  animation:gsap.to('.progress',{scaleX:1,ease:'none'}),
+  once:true,
+  onUpdate:({progress})=>{
+    document.querySelector('.percent span').textContent = Math.round(progress * 100);
+  },
+  pin:true,
+  markers:true,
+  scrub:true,
+})
 
 
 markers()
-
-
