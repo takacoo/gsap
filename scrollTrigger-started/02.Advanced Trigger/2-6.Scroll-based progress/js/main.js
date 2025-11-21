fa8c6a6d63e253e54d99aa6d4a44262f4a244912
ayano takaishi <>
2025-11-21 18:31:12 +0900

    途中まで

--- scrollTrigger-started/02.Advanced Trigger/2-6.Scroll-based progress/js/main.js
+++ scrollTrigger-started/02.Advanced Trigger/2-6.Scroll-based progress/js/main.js
@@ -9,9 +9,52 @@ ScrollTrigger.create({
     document.querySelector('.percent span').textContent = Math.round(progress * 100);
   },
   pin:true,
-  markers:true,
+  // markers:true,
   scrub:true,
 })
+
+const circle = document.querySelector('.circleContainer circle');
+const rect = document.querySelector('.rectContainer rect');
+
+const circleLength = circle.getTotalLength() + 1;
+const rectLength = rect.getTotalLength() + 1;
+
+// gsap.set(circle, {
+//   strokeDashoffset: circleLength,
+//   strokeDasharray: circleLength,
+// });
+
+// gsap.set(rect, {
+//   strokeDashoffset: rectLength,
+//   strokeDasharray: rectLength,
+// });
+
+// const progressSVG = gsap.timeline({
+//   defaults: {
+//     strokeDashoffset: 0,
+//     ease: 'none',
+//   }
+// })
+// .to(circle, {})
+// .to(rect, {}, 0);
+
+// プラグイン使用
+const progressSVG = gsap.timeline({
+  defaults: {
+    ease: 'none',
+  }
+})
+.from(circle, {drawSVG:0})
+.to(rect, {drawSVG:0}, 0)
+
+ScrollTrigger.create({
+  trigger: '.scroll-content',
+  start: 'top top',
+  end: 'bottom bottom',
+  animation: progressSVG,
+  scrub: true,
+  markers: true,
+});
 
 
 markers()
--- scrollTrigger-started/02.Advanced Trigger/2-7.Animation Toggle/js/main.js
+++ scrollTrigger-started/02.Advanced Trigger/2-7.Animation Toggle/js/main.js
@@ -1,15 +1,9 @@
-
-
-
-
-
-
-
-
-
+ScrollTrigger.create({
+  trigger: '.scroll-content',
+  start: 'top top',
+  end: 'bottom bottom',
+  animation:gsap.from('.progress',{scaleY:0,transformOrigin:'center top',ease:'none'}),
+  scrub:true,
+})
 
 markers();
-
-
-
-
